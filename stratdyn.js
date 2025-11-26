dce55480bed35bcf61cfad98ba96360fdb868715
Mobinzarreh <mzarreh@asu.edu>
2025-11-26 15:08:35 -0700

    Clean up for pilot testing: delete all CSV files and set sessionId to 'session1_pilot'

--- stratdyn.js
+++ stratdyn.js
@@ -56,7 +56,7 @@ module.exports = function(io) {
     const userTaskCompletion = {};
 
     let timestamp = Math.floor(new Date().getTime() / 1000);
-    let sessionId = 'session1'; // Can be changed as needed
+    let sessionId = 'session1_pilot'; // Can be changed as needed
 
     // Ensure logs directory exists
     const logsDir = './logs';
