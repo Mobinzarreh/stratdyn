558d1b270ea6b8f9cf8f99c26fcf993d0d0059d5
Mobinzarreh <mzarreh@asu.edu>
2026-02-03 12:54:24 -0700

    Change session ID to session2_pilot for second pilot study

--- stratdyn.js
+++ stratdyn.js
@@ -260,7 +260,7 @@ module.exports = function(io) {
     let userTaskCompletion = {};
 
     let timestamp = Math.floor(new Date().getTime() / 1000);
-    let sessionId = 'session1_pilot'; // Can be changed as needed
+    let sessionId = 'session2_pilot'; // Can be changed as needed
 
     // Ensure logs directory exists
     const logsDir = './logs';
