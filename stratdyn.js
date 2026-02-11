94fc9620cd99e8cce4899b8e4fc3472c0e2a2f63
Mobinzarreh <mzarreh@asu.edu>
2026-02-11 10:21:13 -0700

    Update session ID to session3_pilot for 3rd pilot test

--- stratdyn.js
+++ stratdyn.js
@@ -267,7 +267,7 @@ module.exports = function(io) {
     let trainingCompleteAck = {};
 
     let timestamp = Math.floor(new Date().getTime() / 1000);
-    let sessionId = 'session2_pilot'; // Can be changed as needed
+    let sessionId = 'session3_pilot'; // Can be changed as needed
 
     // Ensure logs directory exists
     const logsDir = './logs';
