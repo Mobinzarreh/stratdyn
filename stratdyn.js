3f64767ac4b01f558d8719b9ea471ace549ceae1
Mobinzarreh <mzarreh@asu.edu>
2026-04-25 11:40:27 -0700

    Set sessionId to session13 for experiment

--- stratdyn.js
+++ stratdyn.js
@@ -289,7 +289,7 @@ module.exports = function(io) {
     let trainingCompleteAck = {};
 
     let timestamp = Math.floor(new Date().getTime() / 1000);
-    let sessionId = 'session12'; // Can be changed as needed
+    let sessionId = 'session13'; // Can be changed as needed
 
     // Ensure logs directory exists
     const logsDir = './logs';
